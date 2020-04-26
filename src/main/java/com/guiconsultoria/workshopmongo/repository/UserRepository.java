ca90ef978aa476f4add86a1f5107ee8f2d0c9f8e
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-26 12:59:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/guiconsultoria/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.guiconsultoria.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guiconsultoria.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
---
+++ src/main/java/com/guiconsultoria/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.guiconsultoria.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guiconsultoria.workshopmongo.domain.User;
+import com.guiconsultoria.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
