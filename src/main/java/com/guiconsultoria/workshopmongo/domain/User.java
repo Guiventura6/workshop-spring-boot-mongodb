d99f29e40a6e88352a85dc534fde3f0f3503ddc3
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-04-26 17:46:59 -0300

    Referenciando os posts do usuário

--- src/main/java/com/guiconsultoria/workshopmongo/domain/User.java
+++ src/main/java/com/guiconsultoria/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.guiconsultoria.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection= "user")
@@ -13,6 +16,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>(); 
 	
 	public User() {
 	}
@@ -48,6 +54,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,6 +86,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 }
